21ee2737b169ab9ccb89ee0934ae81230a8607b7
zoep <zoe.paraskevopoulou@gmail.com>
2026-01-13 19:37:25 +0200

    Nit in entail

--- src/Act/Entailment.hs
+++ src/Act/Entailment.hs
@@ -199,3 +199,17 @@ getModel locs calldataVars ethVars solver = do
         , _menvironment = environment
         , _minitargs = []
         }
+
+
+
+-- For debugging purposes
+
+showCnstr :: Constraint Timed -> String
+showCnstr (BoolCnstr _ msg env e) = "Boolean constraint:\n" <> 
+  msg <> "\n"
+    <> "Preconditions: \n" <> intercalate "\n" (map (\pre -> "  " <>  prettyExp pre) (setPre <$> preconds env)) <> "\n"
+    <> "Expression: " <> prettyExp e <> "\n"
+showCnstr (CallCnstr _ msg _ _ _) = "Call constraint: " <> msg
+    
+showCnstrs :: [Constraint Timed] -> String
+showCnstrs cs = intercalate "\n\n" (map showCnstr cs)
